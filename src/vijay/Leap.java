378ae02f029ae1d0d6673b6322e179060c0229d6
gokularun001 <gokularun20030603@gmail.com>
2024-12-29 02:44:25 -0800

    Task has been completed

---
+++ src/vijay/Leap.java
@@ -0,0 +1,14 @@
+package vijay;
+
+public class Leap
+{
+    public static void main(String[]args){
+        int year=2003;
+        if(year%4==0){
+            System.out.println("It is a leap year");
+        }
+        else{
+            System.out.println("It is not a leap year");
+        }
+    }
+}
