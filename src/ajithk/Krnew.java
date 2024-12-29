2df0625a30ea461432da379acbbdc040306e83fc
ajithkuman <kumar.sppa49@gmail.com>
2024-12-29 01:49:33 -0800

    task finish

---
+++ src/ajithk/Krnew.java
@@ -0,0 +1,14 @@
+package ajithk;
+
+public class Krnew {
+        public static void main(String[] args){
+
+        int number =12;
+        if (number % 6==0){
+            System.out.println("even");
+        }
+        else {
+            System.out.println("odd");
+        }
+            }
+    }
