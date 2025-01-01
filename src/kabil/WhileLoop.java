332f03f6b77cb008659f29a169500148d5fdc4a6
kabil2024 <kabilarasan44@gmail.com>
2025-01-01 21:05:27 +0530

    new files of java

---
+++ src/kabil/WhileLoop.java
@@ -0,0 +1,14 @@
+package kabil;
+
+public class WhileLoop {
+    public static void main(String[] args){
+        int a = 1;
+        while (  a  <= 10 ){
+            System.out.println(a);
+            a+=2;
+            }
+
+
+
+    }
+}
