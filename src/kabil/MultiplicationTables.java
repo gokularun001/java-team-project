2a907ca40beec2f8cc573476bcf7552ba1a0e9a1
Kabil2024 <kabilarasan44@gmail.com>
2025-01-02 14:52:05 +0530

    multiplication tables using while loop

---
+++ src/kabil/MultiplicationTables.java
@@ -0,0 +1,12 @@
+package kabil;
+
+public class MultiplicationTables {
+    public static void main(String[] args){
+        int N = 6;
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(N+ " x " +i +" = " +(N * i));
+            i++;
+        }
+        }
+}
