8c5b473b83a4d49abdcf54c7a140db736eea728b
Kabil2024 <kabilarasan44@gmail.com>
2025-01-07 21:39:51 +0530

    array

---
+++ src/kabil/ArrayPractices.java
@@ -0,0 +1,10 @@
+package kabil;
+
+public class ArrayPractices {
+    public static void main(String[] args) {
+        int a[]= {100,200,300,400};
+        for (int x : a)
+        System.out.println(a);
+
+    }
+}
