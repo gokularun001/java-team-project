6fd0c2d503f7eb1e0514c6b89a2659b2fe8cd800
Kabil2024 <kabilarasan44@gmail.com>
2025-01-07 21:41:38 +0530

    hel

--- src/kabil/JumpStatements.java
+++ src/kabil/JumpStatements.java
@@ -8,6 +8,13 @@ public class JumpStatements {
             }
             System.out.println(a);
         }
+        //next
+        int a = 1,b= 2 ,c = 4;
+        if (a>b && a>c){
+            System.out.println("a is a bigger nnumber");
+        } else if (b>a && b>c) {
+            System.out.println("b is a bigger number");
+        }else System.out.println("c is greater");
 
 
     }
