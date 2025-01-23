d38e024ade3749f1b64bf5396306656ccac33c4e
ajithkuman <kumar.sppa49@gmail.com>
2025-01-23 06:42:38 -0800

    all fils

--- src/ajithk/PraticsTwo.java
+++
@@ -1,9 +0,0 @@
-public class PraticsTwo {
-    public static void main(String[] args) {
-
-        String a ="ajay";
-       for (int i = a.length()-1;i>=0;i--){
-           System.out.println(a.charAt(i));
-       }
-    }
-}
---
+++ src/ajithk/TodayPratics.java
@@ -0,0 +1,15 @@
+package ajithk;
+
+public class TodayPratics {
+    public static void main(String[] args) {
+
+
+        String a = "kabil@gmil.com";
+
+      String z[]  =a.split("@");
+
+        System.out.println(z[0]);
+
+
+    }
+}
