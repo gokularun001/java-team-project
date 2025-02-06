6ff2679fd896cf578e591ec7f8356303bd3865cc
gokularun001 <gokularun20030603@gmail.com>
2025-02-06 12:19:58 -0800

    add Exception and Task

---
+++ src/gokul/Animals.java
@@ -0,0 +1,15 @@
+package gokul;
+
+public class Animals
+{
+    void animal(){
+        System.out.println("Two Types of gokul.Animals");
+        System.out.println("Wild and Domestic");
+    }
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.lion();
+        Tiger t = new Tiger();
+        t.goat();
+    }
+}
---
+++ src/gokul/Tiger.java
@@ -0,0 +1,7 @@
+package gokul;
+
+public class Tiger extends Dog {
+    void goat(){
+        System.out.println("Goat is a Domestic gokul.Animals");
+    }
+}
